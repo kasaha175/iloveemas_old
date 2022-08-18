64a4e64f98f43961e5e5fd9e29c4496a80660671
Zainal Abidin <zainn1303@gmail.com>
2022-08-18 12:23:08 +0700

    fix model

--- application/models/TransactionModel.php
+++ application/models/TransactionModel.php
@@ -1,24 +1,30 @@
 <?php
- if (!defined('BASEPATH'))
- 	exit('No direct script access allowed');
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
-class TransactionModel extends CI_Model {
+class TransactionModel extends CI_Model
+{
 
-	function buyCheckout($data){
+	function buyCheckout($data)
+	{
 		$this->db->insert('tb_transaction', $data);
 		return $this->db->insert_id();
 	}
-	function buyCheckoutItems($data){
+	function buyCheckoutItems($data)
+	{
 		$this->db->insert('tb_transaction_items', $data);
 	}
-	function sellCheckout($data){
+	function sellCheckout($data)
+	{
 		$this->db->insert('tb_transaction_sell', $data);
 		return $this->db->insert_id();
 	}
-	function sellCheckoutItems($data){
+	function sellCheckoutItems($data)
+	{
 		$this->db->insert('tb_transaction_items_sell', $data);
 	}
-	function lastData($year){
+	function lastData($year)
+	{
 		$query = $this->db->query("SELECT a.t_id
 		FROM tb_transaction a
 		WHERE YEAR(a.t_date_created) = '$year'
@@ -26,7 +32,8 @@ class TransactionModel extends CI_Model {
 		LIMIT 1");
 		return $query;
 	}
-	function lastDataSell($year){
+	function lastDataSell($year)
+	{
 		$query = $this->db->query("SELECT a.t_id
 		FROM tb_transaction_sell a
 		WHERE YEAR(a.t_date_created) = '$year'
@@ -34,8 +41,9 @@ class TransactionModel extends CI_Model {
 		LIMIT 1");
 		return $query;
 	}
-	function buyTransaction($dateStart, $dateEnd){
-		if(!empty($dateStart) && !empty($dateEnd)){
+	function buyTransaction($dateStart, $dateEnd)
+	{
+		if (!empty($dateStart) && !empty($dateEnd)) {
 			$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer
 			FROM tb_transaction a
 			LEFT OUTER JOIN tb_user b
@@ -46,7 +54,7 @@ class TransactionModel extends CI_Model {
 			ON a.t_customer = d.c_id
 			WHERE a.t_visible=1 AND DATE(a.t_date_created) >= '$dateStart' AND DATE(a.t_date_created) <= '$dateEnd' 
 			ORDER BY a.t_id DESC");
-		}else{
+		} else {
 			$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer
 			FROM tb_transaction a
 			LEFT OUTER JOIN tb_user b
@@ -60,8 +68,9 @@ class TransactionModel extends CI_Model {
 		}
 		return $query;
 	}
-	function sellTransaction($dateStart, $dateEnd){
-		if(!empty($dateStart) && !empty($dateEnd)){
+	function sellTransaction($dateStart, $dateEnd)
+	{
+		if (!empty($dateStart) && !empty($dateEnd)) {
 			$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer
 			FROM tb_transaction_sell a
 			LEFT OUTER JOIN tb_user b
@@ -72,7 +81,7 @@ class TransactionModel extends CI_Model {
 			ON a.t_customer = d.c_id
 			WHERE a.t_visible=1 AND DATE(a.t_date_created) >= '$dateStart' AND DATE(a.t_date_created) <= '$dateEnd' 
 			ORDER BY a.t_id DESC");
-		}else{
+		} else {
 			$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer
 			FROM tb_transaction_sell a
 			LEFT OUTER JOIN tb_user b
@@ -86,14 +95,16 @@ class TransactionModel extends CI_Model {
 		}
 		return $query;
 	}
-	function sellDeleteTransaction($idTransaction){
+	function sellDeleteTransaction($idTransaction)
+	{
 		$query = $this->db->query("UPDATE
 		tb_transaction_sell a
 		SET a.t_visible=0
 		WHERE a.t_id='$idTransaction'");
 		return $query;
 	}
-	function buyTransactionGraph($year,$material){
+	function buyTransactionGraph($year, $material)
+	{
 		$query = $this->db->query("SELECT *
 			FROM
 			(SELECT *
@@ -108,11 +119,12 @@ class TransactionModel extends CI_Model {
 			ON x.m_id = y.m_id");
 		return $query;
 	}
-	function buyTransactionCustomerGraph($year){
+	function buyTransactionCustomerGraph($year)
+	{
 		$bulan = $this->db->query("SELECT a.m_id, a.m_name as month FROM tb_month a")->result();
 		$data = array();
 		foreach ($bulan as $key => $value) {
-			$query = $this->db->query("SELECT COUNT(tb_transaction.t_customer) as countTransaction,m_id FROM `tb_transaction` inner join `tb_customer` right outer join `tb_month` ON tb_transaction.t_customer = tb_customer.c_id AND YEAR(`t_date_created`) = '$year' AND MONTH(tb_transaction.t_date_created) =".$value->m_id)->row();
+			$query = $this->db->query("SELECT COUNT(tb_transaction.t_customer) as countTransaction,m_id FROM `tb_transaction` inner join `tb_customer` right outer join `tb_month` ON tb_transaction.t_customer = tb_customer.c_id AND YEAR(`t_date_created`) = '$year' AND MONTH(tb_transaction.t_date_created) =" . $value->m_id)->row();
 			array_push($data, [
 				'm_id' => $query->m_id,
 				'countTransaction' => $query->countTransaction,
@@ -122,7 +134,8 @@ class TransactionModel extends CI_Model {
 		}
 		return $data;
 	}
-	function sellTransactionGraph($year,$material){
+	function sellTransactionGraph($year, $material)
+	{
 		$query = $this->db->query("SELECT *
 			FROM
 			(SELECT *
@@ -137,11 +150,12 @@ class TransactionModel extends CI_Model {
 			ON x.m_id = y.m_id");
 		return $query;
 	}
-	function sellTransactionCustomerGraph($year){
+	function sellTransactionCustomerGraph($year)
+	{
 		$bulan = $this->db->query("SELECT a.m_id, a.m_name as month FROM tb_month a")->result();
 		$data = array();
 		foreach ($bulan as $key => $value) {
-			$query = $this->db->query("SELECT COUNT(tb_transaction_sell.t_customer) as countTransaction,m_id FROM `tb_transaction_sell` inner join `tb_customer` right outer join `tb_month` ON tb_transaction_sell.t_customer = tb_customer.c_id AND YEAR(`t_date_created`) = '$year' AND MONTH(tb_transaction_sell.t_date_created) =".$value->m_id)->row();
+			$query = $this->db->query("SELECT COUNT(tb_transaction_sell.t_customer) as countTransaction,m_id FROM `tb_transaction_sell` inner join `tb_customer` right outer join `tb_month` ON tb_transaction_sell.t_customer = tb_customer.c_id AND YEAR(`t_date_created`) = '$year' AND MONTH(tb_transaction_sell.t_date_created) =" . $value->m_id)->row();
 			array_push($data, [
 				'm_id' => $query->m_id,
 				'countTransaction' => $query->countTransaction,
@@ -151,14 +165,17 @@ class TransactionModel extends CI_Model {
 		}
 		return $data;
 	}
-	function buyDeleteTransaction($idTransaction){
+	function buyDeleteTransaction($idTransaction)
+	{
 		$query = $this->db->query("UPDATE
 		tb_transaction a
 		SET a.t_visible=0
 		WHERE a.t_id='$idTransaction'");
 		return $query;
 	}
-	function buyTransactionData($idTransaction){
+	function buyTransactionData($idTransaction)
+	{
+		/*
 		$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer, d.*
 		FROM tb_transaction a
 		LEFT OUTER JOIN tb_user b
@@ -168,9 +185,22 @@ class TransactionModel extends CI_Model {
 		LEFT OUTER JOIN tb_customer d
 		ON a.t_customer = d.c_id
 		WHERE a.t_id='$idTransaction' AND a.t_visible=1");
+		*/
+
+		$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer, d.*
+		FROM tb_transaction a
+		LEFT OUTER JOIN tb_user b
+		ON a.t_created_by = b.u_id
+		LEFT OUTER JOIN tb_user c
+		ON a.t_receive_by = c.u_id
+		LEFT OUTER JOIN tb_customer d
+		ON a.t_customer = d.c_id
+		WHERE a.t_id='$idTransaction'");
+
 		return $query;
 	}
-	function buyTransactionItemsData($idTransaction){
+	function buyTransactionItemsData($idTransaction)
+	{
 		$query = $this->db->query("SELECT a.*
 		FROM tb_transaction_items a
 		LEFT OUTER JOIN tb_transaction b
@@ -178,7 +208,9 @@ class TransactionModel extends CI_Model {
 		WHERE a.ti_t_id='$idTransaction'  AND b.t_visible=1");
 		return $query;
 	}
-	function sellTransactionData($idTransaction){
+	function sellTransactionData($idTransaction)
+	{
+		/*
 		$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer, d.*
 		FROM tb_transaction_sell a
 		LEFT OUTER JOIN tb_user b
@@ -189,9 +221,22 @@ class TransactionModel extends CI_Model {
 		ON a.t_customer = d.c_id
 		
 		WHERE a.t_id='$idTransaction' AND a.t_visible=1");
+		*/
+
+		$query = $this->db->query("SELECT a.*, b.u_name as nameCreator, c.u_name as nameReceive, d.c_name as nameCustomer, d.*
+		FROM tb_transaction_sell a
+		LEFT OUTER JOIN tb_user b
+		ON a.t_created_by = b.u_id
+		LEFT OUTER JOIN tb_user c
+		ON a.t_receive_by = c.u_id
+		LEFT OUTER JOIN tb_customer d
+		ON a.t_customer = d.c_id
+		
+		WHERE a.t_id='$idTransaction'");
 		return $query;
 	}
-	function sellTransactionItemsData($idTransaction){
+	function sellTransactionItemsData($idTransaction)
+	{
 		$query = $this->db->query("SELECT a.*
 		FROM tb_transaction_items_sell a
 		LEFT OUTER JOIN tb_transaction_sell b
@@ -199,6 +244,4 @@ class TransactionModel extends CI_Model {
 		WHERE a.ti_t_id='$idTransaction'  AND b.t_visible=1");
 		return $query;
 	}
-
 }
-?>
