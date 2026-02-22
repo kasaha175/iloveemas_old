df52a2df47d6caae7d3007c96c2f43a258e5863c
kasahana175 <kasahana4@gmail.com>
2026-02-22 12:57:06 +0700

    implementasi login attempt dan session destroy

--- application/views/Login.php
+++ application/views/Login.php
@@ -78,10 +78,10 @@
         <?php if($this->session->userdata("failedLogin")==true){ ?>
         <div class="error-msg">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><line x1="12" x2="12" y1="8" y2="12"/><line x1="12" x2="12.01" y1="16" y2="16"/></svg>
-            <span>Username atau password salah!</span>
+            <span><?php echo $this->session->userdata("lockout_message") ?: "Username atau password salah!"; ?></span>
         </div>
         <?php 
-            $this->session->set_userdata(['failedLogin'=>false]);
+            $this->session->set_userdata(['failedLogin'=>false, 'lockout_message'=>null]);
         } ?>
 
         <!-- Version -->
