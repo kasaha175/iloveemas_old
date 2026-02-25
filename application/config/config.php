130b5f94bda27783f83e1806724be7cdb395d159
kasahana175 <kasahana4@gmail.com>
2026-02-26 00:17:36 +0700

    perbaikan base_url dan htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 // $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 // $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = 'http://103.27.207.23/';
+// $config['base_url'] = 'http://103.27.207.23/';
+$config['base_url'] = 'http://103.27.207.23:8082/';
+
 $config['index_page'] = '';
 
 /*
