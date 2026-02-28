57351201ce2517e202142f62131018d4b12050e9
kasahana175 <kasahana4@gmail.com>
2026-03-01 02:04:26 +0700

    sesuaikan style halaman login

--- application/views/Login.php
+++ application/views/Login.php
@@ -67,12 +67,11 @@
             <button type="submit" class="btn-login" id="loginBtn">
                 <span class="btn-text">Masuk</span>
                 <span class="btn-loader">
-                    <svg class="spinner" viewBox="0 0 50 50">
-                        <circle cx="25" cy="25" r="20" fill="none" stroke-width="4"></circle>
-                    </svg>
+                    <span class="spinner"></span>
                 </span>
-                <svg class="btn-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
             </button>
+
+            <div class="login-toast" id="loginToast"></div>
 
         </form>
 
